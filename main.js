f1585fb85406d4bc90a37254f8d7e6cd6ed68244
mcalsing <marcelocalsing@gmail.com>
2024-12-28 18:07:18 -0300

    feat: Adicionado a mecânica de batalha

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ const spearr = Cartas.spearr
 const horsee = Cartas.horsee
 const archer = Cartas.archer
 
+let grid = [
+  {position: 0},
+  {position: 1},
+  {position: 2},
+  {position: 3},
+  {position: 4},
+  {position: 5},
+  {position: 6},
+  {position: 7},
+  {position: 8},
+]
+
+const adjacencyMap = {
+  0: { leste: 1, sul: 3 },
+  1: { oeste: 0, leste: 2, sul: 4 },
+  2: { oeste: 1, sul: 5 },
+  3: { norte: 0, leste: 4, sul: 6 },
+  4: { norte: 1, oeste: 3, leste: 5, sul: 7 },
+  5: { norte: 2, oeste: 4, sul: 8 },
+  6: { norte: 3, leste: 7 },
+  7: { norte: 4, oeste: 6, leste: 8 },
+  8: { norte: 5, oeste: 7 }
+};
+
+
+
 function createDiv(quant, name, where) {
   for (let i = 0; i < quant; i++) {
     let divGrid = document.createElement('div')
@@ -37,18 +63,6 @@ function cardFromHand(eventOrigin) {
   }
 }
 
-let grid = [
-  {position: 0, },
-  {position: 1},
-  {position: 2},
-  {position: 3},
-  {position: 4},
-  {position: 5},
-  {position: 6},
-  {position: 7},
-  {position: 8},
-]
-
 const insertCardOnBoard = document.querySelector('#main-board')
 insertCardOnBoard.addEventListener('click', function(eventOrigin) {
   let currentPositionBoard = eventOrigin.target.className.slice(-1)
@@ -68,14 +82,17 @@ insertCardOnBoard.addEventListener('click', function(eventOrigin) {
       }
   
       grid[currentPositionBoard] = {character: character.nome, norte: character.norte, leste: character.leste, sul: character.sul, oeste: character.oeste}
-      for (let i = 0; i < 9; i++) {
+     /*  for (let i = 0; i < 9; i++) {
         console.log(grid[i])
-      }
+      } */
   
+      compareAdjacentCards(currentPositionBoard)
+
+
       selectedCard = null
     } else {
       /* alert("Nenhuma carta selecionada") */
-      alert("Nenhuma carta selecionada")
+      alert("Selecione uma carta da mão antes de coloca-lá no Board")
     }
 
   } else {
@@ -83,6 +100,37 @@ insertCardOnBoard.addEventListener('click', function(eventOrigin) {
   }
 
 })
+
+function compareAdjacentCards(current) {
+  const adjacents = adjacencyMap[current]
+  //console.log(adjacents)
+
+  for (const direction in adjacents) {
+    const adjacentPosition = adjacents[direction]
+    const adjacentCard = grid[adjacentPosition] // Obtem as cartas adjacentes
+    //console.log('Onde a carta colocada tem adjacente:', direction)
+
+    //console.log(Object.keys(grid[adjacentPosition])[0])
+    if (Object.keys(grid[adjacentPosition])[0] !== 'position') {
+      const oppositeDirection = {
+        norte: "sul",
+        sul: "norte",
+        leste: "oeste",
+        oeste: "leste"
+      }[direction];
+
+      const currentCardValue = grid[current][direction]
+      
+      //console.log('Tem carta no lado:', oppositeDirection)
+      const adjacentCardValue = adjacentCard[oppositeDirection]
+      //console.log(`em um ${adjacentCardValue} no ${oppositeDirection}`)
+
+      if (currentCardValue > adjacentCardValue) {
+        console.log('vc ganhou')
+      }
+    }
+  }
+}
 
 let randomCards = [spearr, horsee, archer]
 function randomCard() {
@@ -92,8 +140,6 @@ function randomCard() {
 }
 
 function cardsOnPlayerHand() {
-
-  console.log(`"url('${spearr.imagem}')"`)
   let hand0 = document.querySelector(".hand0").style.backgroundImage = `url('${spearr.imagem}')`
   let hand1 = document.querySelector(".hand1").style.backgroundImage = `url('${horsee.imagem}')`
   let hand2 = document.querySelector(".hand2").style.backgroundImage = `url('${archer.imagem}')`
